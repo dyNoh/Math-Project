c01df6f3e06f1f998df23667cec770393fad76f4
2taehun <lth527@naver.com>
2016-10-24 16:58:03 +0900

    Update main.c

--- main.c
+++ main.c
@@ -5,6 +5,6 @@ int main(void)
   printf("Open Source SW Project\n");
   printf("First Github Training\n");
   printf("2014244013 Daeyeon Noh\n");
-  printf("x + y = 5");
+  printf("2012244072 TaeHun Lee\n");
   return 0;
 }
