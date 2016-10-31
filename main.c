5e0bc575f1787837671a71369849817199e7827a
2taehun <lth5296@gmail.com>
2016-10-31 17:09:36 +0900

    Update main.c

--- main.c
+++ main.c
@@ -6,6 +6,19 @@ int main(void)
   printf("First Github Training\n");
   printf("2014244013 Daeyeon Noh\n");
   printf("2012244072 TaeHun Lee\n");
-   printf("수학재밌는수\n");
+  printf("수학재밌는수\n");
+
+  int x =0, y=0;
+
+  printf("더하기 입력 : ");
+  scanf("%d", &x);
+  scanf("%d", &y);
+
+  int result = 0;
+
+  result = x+y;
+
+  printf("더해진 수는 %d 입니다.", result);
+
   return 0;
 }
